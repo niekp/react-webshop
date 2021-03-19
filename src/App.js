c11a92671f1200a34c1bc2260f2b3054f7b07ea0
Niek <git@niekvantoepassing.nl>
2021-03-19 14:09:16 +0100

    Add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
 
 function App() {
   return (
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
-        
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+    <div className={`${size} menu-item`} 
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
         <div className='background-image' style={{
             backgroundImage: `url(${imageUrl})`
         }} ></div>
-        
+
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
@@ -15,4 +17,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
