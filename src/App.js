a64c61b26727de218f2a4e514d0a7dd35e670a60
Niek <git@niekvantoepassing.nl>
2021-03-19 14:50:42 +0100

    Add shop collection preview & item components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import { Route, Switch, Link } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
