834bf364263507c4a0cd3f2ac2b8878351f01cb0
Niek <git@niekvantoepassing.nl>
2021-03-20 11:22:18 +0100

    Store user in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,7 +11,36 @@ const config = {
     appId: "1:797260358409:web:a7b56c1efdb164399469d8"
 };
 
-firebase.initializeApp(config);
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+} else {
+    firebase.app();
+}
+
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if (!userAuth) return;
+    
+    const userRef = firestore.collection('users').doc(userAuth.uid);
+    const snapshot = await userRef.get();
+
+    if (!snapshot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName: displayName,
+                email: email,
+                createdAt: createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.error('error creating user', error);
+        }
+    }
+
+    return userRef;
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
